dd7fa14a7bda4dd3f79ae258693f30508dab171f
zhaoshun2000 <qq17116500520@163.com>
2020-08-19 14:39:53 +0800

    测试分支上ç传

---
+++ src/main/java/com/jk/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : Zs
+ * @date : 14:37 2020/8/19
+ * @user : zhaoshun
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    public void hello(){
+
+    }
+
+}
