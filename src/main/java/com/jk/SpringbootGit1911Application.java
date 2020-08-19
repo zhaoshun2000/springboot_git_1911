fd45b8d4f17a14ac2d2cd49c8b35fb16a65abc45
zhaoshun2000 <qq17116500520@163.com>
2020-08-19 14:26:37 +0800

    第一次初始化

---
+++ src/main/java/com/jk/SpringbootGit1911Application.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGit1911Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGit1911Application.class, args);
+    }
+
+}
